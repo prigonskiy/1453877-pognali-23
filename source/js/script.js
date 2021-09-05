2438d3126a2371d7a22336adb110abbe79d66dd3
Filipp Prigonskiy <jerk1991@gmail.com>
2021-09-05 22:25:32 +0300

    Доделал мобильное отображение index.html, включая perfect pixel

--- source/js/script.js
+++ source/js/script.js
@@ -6,9 +6,10 @@ let deviceWidth = document.body.clientWidth;
 
 let index = document.querySelector('.page--index');
 let form = document.querySelector('.page--form');
+let catalog = document.querySelector('.page--catalog');
 
 // Menu
-
+/*
 let menu = document.querySelector('.page-header__menu');
 let menuWrapper = menu.querySelector('.page-header__menu-wrapper');
 let menuLogoImage = menu.querySelector('.page-header__logo-image');
@@ -16,28 +17,25 @@ let menuToggle = menu.querySelector('.page-header__menu-toggle');
 
 menu.classList.add('page-header__menu--closed');
 menuWrapper.classList.add('page-header__menu-wrapper--closed');
-menuLogoImage.classList.add('page-header__logo-image--closed');
 menuToggle.classList.add('page-header__menu-toggle--closed');
 
 menuToggle.addEventListener('click', function(evt) {
   evt.preventDefault();
-  if (menuToggle.classList.contains('page-header__menu-toggle--closed')) {
+  if (menuToggle.classList.contains('page-header__menu-toggle--closed') ) {
     menu.classList.remove('page-header__menu--closed');
     menu.classList.add('page-header__menu--active');
     menuWrapper.classList.remove('page-header__menu-wrapper--closed');
-    menuLogoImage.classList.remove('page-header__logo-image--closed');
     menuToggle.classList.remove('page-header__menu-toggle--closed');
     menuToggle.classList.add('page-header__menu-toggle--active');
   } else {
     menu.classList.remove('page-header__menu--active');
     menu.classList.add('page-header__menu--closed');
     menuWrapper.classList.add('page-header__menu-wrapper--closed');
-    menuLogoImage.classList.add('page-header__logo-image--closed');
     menuToggle.classList.remove('page-header__menu-toggle--active');
     menuToggle.classList.add('page-header__menu-toggle--closed');
   }
 });
-
+*/
 // Business-modal
 
 if (index) {
@@ -58,16 +56,39 @@ if (index) {
   })
 }
 
-// Choose country {
+// Companion filter {
 
-/*if (form) {
-  let countryPickers = document.querySelectorAll('.country-picker country-picker--choose');
-  let addCountries = countryPickers.querySelectorAll('.country-picker__button');
+if (catalog) {
+  let filterFieldsets = document.querySelectorAll('.companion-filter__fieldset');
 
-  for (let i = 0; i < countryPickers.length; i++) {
-    addCountries[i].addEventListener('click', function(evt) {
-      evt.preventDefault();
-      countryPickers[i]
+  for (let i = 0; i < filterFieldsets.length; i++) {
+    filterFieldsets[i].classList.remove('companion-filter__fieldset--nojs');
+    filterFieldsets[i].addEventListener('click', function() {
+      if (filterFieldsets[i].classList.contains('companion-filter__fieldset--hidden')) {
+        filterFieldsets[i].classList.remove('companion-filter__fieldset--hidden');
+      } else {
+        filterFieldsets[i].classList.add('companion-filter__fieldset--hidden')
+      }
     })
   }
-}*/
+}
+
+// Likes toggler
+
+if (catalog) {
+  let likesTogglers = document.querySelectorAll('.companion-cards__likes-toggle');
+  let likesIcons = document.querySelectorAll('.companion-cards__likes-icon');
+
+  for (let i = 0; i < likesTogglers.length; i++) {
+    likesTogglers[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      if (likesTogglers[i].classList.contains('companion-cards__likes-toggle--active')) {
+        likesTogglers[i].classList.remove('companion-cards__likes-toggle--active');
+        likesIcons[i].classList.remove('companion-cards__likes-icon--active')
+      } else {
+        likesTogglers[i].classList.add('companion-cards__likes-toggle--active');
+        likesIcons[i].classList.add('companion-cards__likes-icon--active')
+      }
+    })
+  }
+}
